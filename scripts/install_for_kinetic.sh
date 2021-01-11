d5c69d6358d2bf8803020537d106b4515853ce41
zhangwanjie <zhangwanjie@126.com>
2021-01-11 17:23:00 +0800

    first commit

---
+++ scripts/install_for_kinetic.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sudo apt-get install ros-kinetic-desktop-full
+sudo apt-get install ros-kinetic-gazebo-ros-control
+sudo apt-get install ros-kinetic-joint-state-controller
+sudo apt-get install ros-kinetic-position-controllers
+sudo apt-get install ros-kinetic-effort-controllers
+sudo apt-get install ros-kinetic-cv-bridge
+sudo apt-get install ros-kinetic-controller-manager
+sudo apt-get install ros-kinetic-qt-create
+sudo apt-get install ros-kinetic-qt-build
+sudo apt-get install ros-kinetic-joint-state-publisher-gui
+sudo apt-get install ros-kinetic-navigation
---
+++ scripts/install_for_melodic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sudo apt-get install ros-melodic-desktop-full
+sudo apt-get install ros-melodic-gazebo-ros-control
+sudo apt-get install ros-melodic-joint-state-controller
+sudo apt-get install ros-melodic-position-controllers
+sudo apt-get install ros-melodic-effort-controllers
+sudo apt-get install ros-melodic-cv-bridge
+sudo apt-get install ros-melodic-controller-manager
+sudo apt-get install ros-melodic-qt-ros
+sudo apt-get install ros-melodic-joint-state-publisher-gui
+sudo apt-get install ros-melodic-navigation
